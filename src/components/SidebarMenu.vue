44e72162afde5aa8ab13df1cd221e1215e6efca3
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2024-10-06 17:14:41 +0200

    feat: Add @motionone/dom dependency for animation support

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -5,6 +5,13 @@
 </template>
 
 <script setup>
+import { animate } from "@motionone/dom";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  // Anime l'élément quand le composant est monté
+  animate(".sidebar-menu h1", { transform: ["scale(0.5)", "scale(1)"] }, { duration: 1 });
+});
 </script>
 
 <style lang="scss" scoped>
