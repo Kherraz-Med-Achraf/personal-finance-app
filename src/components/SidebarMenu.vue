c24c54d77321b15100b4b35f7fa99a3f351b2d93
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2024-10-06 17:45:10 +0200

    refactor: Update SidebarMenu component
    - Refactor SidebarMenu component to include logo, menu items, and minimize button
    - Add necessary assets for the component
    - Update styles for the sidebar menu

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,23 +1,50 @@
 <template>
-    <div class="sidebar-menu">
-        <h1>Hello World side</h1>
+  <div class="sidebar-menu">
+    <div class="logo">
+      <img v-if="sidebarMenuOpen" :src="LogoLarge" alt="logo" />
+      <img v-else :src="logoSmall" alt="logo" />
+
     </div>
+    <ul>
+      <li>
+        <router-link to="/"
+          ><img :src="OverviewIcon" alt="Overview Icon" /><span v-if="sidebarMenuOpen">Overview</span></router-link
+        >
+      </li>
+      <li>
+        <router-link to="/"
+          ><img :src="TransactionsIcon" alt="Transactions Icon" /><span v-if="sidebarMenuOpen">Transactions</span></router-link
+        >
+      </li>
+    </ul>
+    <button>
+      <img :src="MinimiseIcon" alt="Minimize Menu Icon" />
+      <span v-if="sidebarMenuOpen">Minimize Menu</span>
+    </button>
+  </div>
 </template>
 
 <script setup>
 import { animate } from "@motionone/dom";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
+import LogoLarge from "@/assets/images/logo-large.svg";
+import logoSmall from "@/assets/images/logo-small.svg";
+import OverviewIcon from "@/assets/images/icon-nav-overview.svg";
+import TransactionsIcon from "@/assets/images/icon-nav-transactions.svg";
+import MinimiseIcon from "@/assets/images/icon-minimize-menu.svg";
 
-onMounted(() => {
-  // Anime l'élément quand le composant est monté
-  animate(".sidebar-menu h1", { transform: ["scale(0.5)", "scale(1)"] }, { duration: 1 });
-});
+let sidebarMenuOpen = ref(true);
+
+// onMounted(() => {
+//   // Anime l'élément quand le composant est monté
+//   animate(".sidebar-menu h1", { transform: ["scale(0.5)", "scale(1)"] }, { duration: 1 });
+// });
 </script>
 
 <style lang="scss" scoped>
 .sidebar-menu {
-    h1 {
-        color: #333;
-    }
+  h1 {
+    color: #333;
+  }
 }
 </style>
